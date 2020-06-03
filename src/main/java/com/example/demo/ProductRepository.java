7281bf2b83bf8147e7beed36c84f4b941ef1767e
10098 <chanhee.won@sk.com>
2020-06-03 10:36:30 +0900

    first commit

---
+++ src/main/java/com/example/demo/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+import com.example.demo.Product;
+import org.springframework.data.repository.CrudRepository;
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
